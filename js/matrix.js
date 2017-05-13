6ac9c0d58ec011610b563c1620e84d7542d83677
Chad Vink <chad@carmelsoft.com>
2017-05-12 19:49:37 -0700

    New game logic

--- js/matrix.js
+++ js/matrix.js
@@ -1,12 +1,12 @@
 // data model for v2
 //based on some combination of conway's game of life and probabilistic bizness
 
-var Matrix = function(rows=8, cols=8) {
+var Matrix = function(rows = 8, cols = 8) {
     this.rows = rows;
     this.cols = cols;
     this.matrix;
+    this.seed = this.intToBinary(541);
     this.initializeMatrix();
-
     this.currentCellR = 0;
 }
 
@@ -14,10 +14,10 @@ var Matrix = function(rows=8, cols=8) {
 Matrix.prototype.initializeMatrix = function() {
     this.matrix = new Array();
     for (i = 0; i < this.rows; i++) {
-	this.matrix[i] = new Array();
-	for (j = 0; j < this.cols; j++) {
-	    this.matrix[i][j] = 1;
-	}
+        this.matrix[i] = new Array();
+        for (j = 0; j < this.cols; j++) {
+            this.matrix[i][j] = 1;
+        }
     }
     this.randomStartingState();
 }
@@ -36,13 +36,13 @@ Matrix.prototype.progress = function() {
 Matrix.prototype.stuff = function() {
     var newMatrix = new Array();
     for (i = 0; i < this.rows; i++) {
-	newMatrix[i] = new Array();
-	for (j = 0; j < this.cols; j++) {
-	    newMatrix[i][j] = 0;
-	}
+        newMatrix[i] = new Array();
+        for (j = 0; j < this.cols; j++) {
+            newMatrix[i][j] = 0;
+        }
     }
     for (var i = 0; i < this.cols; i++) {
-	newMatrix[this.currentCellR][i] = 1;
+        newMatrix[this.currentCellR][i] = 1;
     }
     this.currentCellR += 1 % this.rows;
     this.matrix = newMatrix;
@@ -54,41 +54,50 @@ Matrix.prototype.conwaysGOL = function() {
     alive_cells = [];
     var newMatrix = new Array();
     for (i = 0; i < this.rows; i++) {
-	newMatrix[i] = new Array();
-	for (j = 0; j < this.cols; j++) {
-	    newMatrix[i][j] = 0;
-	}
+        newMatrix[i] = new Array();
+        for (j = 0; j < this.cols; j++) {
+            newMatrix[i][j] = 0;
+        }
     }
-    
+
     function _isFilled(matrix, row, col) {
-	return matrix[row] && matrix[row][col];
+        return matrix[row] && matrix[row][col];
     }
+
+
 
     for (i = 0; i < this.rows; i++) {
-	for (j = 0; j < this.cols; j++) {
-	    var neighbors = 0,
-		alive = 0;
-	    
-	    if (_isFilled(this.matrix, i-1, j-1)) neighbors++;
-	    if (_isFilled(this.matrix, i-1, j)) neighbors++;
-	    if (_isFilled(this.matrix, i-1, j+1)) neighbors++;
-	    if (_isFilled(this.matrix, i, j-1)) neighbors++;
-	    if (_isFilled(this.matrix, i, j+1)) neighbors++;
-	    if (_isFilled(this.matrix, i+1, j-1)) neighbors++;
-	    if (_isFilled(this.matrix, i+1, j)) neighbors++;
-	    if (_isFilled(this.matrix, i+1, j+1)) neighbors++;
+        for (j = 0; j < this.cols; j++) {
 
-	    if (this.matrix[i][j]) { // if alive
-		alive = neighbors == 2 || neighbors == 3 ? 1 : 0;
-	    } else {
-		alive = neighbors == 3 ? 1 : 0;
-	    }
-	    newMatrix[i][j] = alive;
+            var neighbors = [0,0,0,0,0,0,0,0,0],
+                alive = 0;
 
-	    if (alive) {
-		alive_cells.push(new Array(i, j));
-	    }
-	}
+            if (_isFilled(this.matrix, i - 1, j - 1)) neighbors[0] = 1;
+            if (_isFilled(this.matrix, i - 1, j)) neighbors[1] = 1;
+            if (_isFilled(this.matrix, i - 1, j + 1)) neighbors[2] = 1;
+            if (_isFilled(this.matrix, i, j - 1)) neighbors[3] = 1;
+            if (_isFilled(this.matrix, i, j)) neighbors[4] = 1;
+            if (_isFilled(this.matrix, i, j + 1)) neighbors[5] = 1;
+            if (_isFilled(this.matrix, i + 1, j - 1)) neighbors[6] = 1;
+            if (_isFilled(this.matrix, i + 1, j)) neighbors[7] = 1;
+            if (_isFilled(this.matrix, i + 1, j + 1)) neighbors[8] = 1;
+
+            alive = this.seed[this.binaryToInt(neighbors)]
+
+            // if (this.matrix[i][j]) { // if alive
+            //     alive = neighbors == 2 || neighbors == 3 ? 1 : 0;
+            // } else {
+            //     alive = neighbors == 3 ? 1 : 0;
+            // }
+            // var m = this.intToBinary(neighbors)
+            // console.log(m);
+            // console.log(this.binaryToInt(m));
+            newMatrix[i][j] = alive;
+
+            if (alive) {
+                alive_cells.push(new Array(i, j));
+            }
+        }
     }
     this.matrix = newMatrix;
     return alive_cells;
@@ -96,10 +105,10 @@ Matrix.prototype.conwaysGOL = function() {
 
 Matrix.prototype.randomStartingState = function() {
     for (i = 0; i < this.rows; i++) {
-	var row = "" + i + ": ";
-	for (j = 0; j < this.cols; j++) {
-	    this.matrix[i][j] = randomInt(0,5) == 0 ? 1 : 0;  // 1 in 4 chaance i think
-	}
+        var row = "" + i + ": ";
+        for (j = 0; j < this.cols; j++) {
+            this.matrix[i][j] = randomInt(0, 5) == 0 ? 1 : 0; // 1 in 4 chaance i think
+        }
     }
 }
 
@@ -107,9 +116,9 @@ Matrix.prototype.randomStartingState = function() {
 Matrix.prototype.rowsInColumn = function(colNum) {
     var cells = []
     for (i = 0; i < this.rows; i++) {
-	if (this.matrix[i][colNum]) {
-	    cells.push(i);
-	}
+        if (this.matrix[i][colNum]) {
+            cells.push(i);
+        }
     }
     return cells;
 }
@@ -118,9 +127,9 @@ Matrix.prototype.rowsInColumn = function(colNum) {
 Matrix.prototype.colsInRow = function(rowNum) {
     var cells = []
     for (i = 0; i < this.cols; i++) {
-	if (this.matrix[rowNum][i]) {
-	    cells.push(i);
-	}
+        if (this.matrix[rowNum][i]) {
+            cells.push(i);
+        }
     }
     return cells;
 }
@@ -128,18 +137,18 @@ Matrix.prototype.colsInRow = function(rowNum) {
 // returns true if row empty
 Matrix.prototype.rowEmpty = function(rowNum) {
     for (i = 0; i < this.cols; i++) {
-	if (this.matrix[rowNum][i] == 1) {
-	    return false;
-	}
+        if (this.matrix[rowNum][i] == 1) {
+            return false;
+        }
     }
     return true
 }
 // returns true if column empty
 Matrix.prototype.colEmpty = function(colNum) {
     for (i = 0; i < this.rows; i++) {
-	if (this.matrix[i][colNum] == 1) {
-	    return false;
-	}
+        if (this.matrix[i][colNum] == 1) {
+            return false;
+        }
     }
     return true
 }
@@ -148,20 +157,20 @@ Matrix.prototype.colEmpty = function(colNum) {
 Matrix.prototype.rowEdges = function() {
     var edges = []
     for (i = 0; i < this.rows; i++) {
-	if (this.matrix[i][0] || this.matrix[i][this.cols-1]) {
-	    edges.push(i);
-	}
+        if (this.matrix[i][0] || this.matrix[i][this.cols - 1]) {
+            edges.push(i);
+        }
     }
     return edges;
 }
 
-// returns list of column edges that are occupied, on either top or bottom 
+// returns list of column edges that are occupied, on either top or bottom
 Matrix.prototype.colEdges = function() {
     var edges = []
     for (i = 0; i < this.cols; i++) {
-	if (this.matrix[0][i] || this.matrix[this.rows-1][i]) {
-	    edges.push(i);
-	}
+        if (this.matrix[0][i] || this.matrix[this.rows - 1][i]) {
+            edges.push(i);
+        }
     }
     return edges;
 }
@@ -170,12 +179,26 @@ Matrix.prototype.colEdges = function() {
 Matrix.prototype.print = function() {
     var total = ""
     for (i = 0; i < this.rows; i++) {
-	//var row = "" + i + ": ";
-	var row = "";
-	for (j = 0; j < this.cols; j++) {
-	    row += this.matrix[i][j] + " ";
-	}
-	total += row + "\n";
+        //var row = "" + i + ": ";
+        var row = "";
+        for (j = 0; j < this.cols; j++) {
+            row += this.matrix[i][j] + " ";
+        }
+        total += row + "\n";
     }
     console.log(total);
+}
+
+Matrix.prototype.intToBinary = function(x) {
+  var binNum = (x >>> 0).toString(2);
+  var arr = binNum.split("").map(Number);
+  for (var i = arr.length; i<9; i++){
+    arr.unshift(0);
+  }
+  return arr;
+}
+
+Matrix.prototype.binaryToInt = function(x) {
+  var val = x.join("");
+  return parseInt(val,2);
 }
